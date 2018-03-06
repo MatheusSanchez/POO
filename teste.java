6231c2f03d3dc57370689eff4fd0b138e1f4f058
Matheus Sanchez <you@example.com>
2018-03-05 22:42:15 -0300

    Entrada com Scanner

--- teste.java
+++ teste.java
@@ -1,9 +1,38 @@
+import java.util.Scanner; // classe scanner para ler dados
+import java.io.IOException;
+
 public class teste {
 
-	public static void main(String[] args) throws Exception {
+	
 
-		
-		System.out.printf("AQUELE OLÁ MUNDO CLÁSSICO\n");
+	public static void main(String[] args) throws Exception {
+		Scanner lendo = new Scanner(System.in); // instanciando objeto tipo scanner
+		int k = 0;
+
+		/*
+			float numF = sc.nextFloat();
+			int num1 = sc.nextInt();
+			byte byte1 = sc.nextByte();
+			long lg1 = sc.nextLong();
+			boolean b1 = sc.nextBoolean();
+			double num2 = sc.nextDouble();
+			String nome = sc.nextLine();
+		*/
+
+		while(k != 1){
+			System.out.printf("Digite 1 para parar o programa: ");
+
+			k = lendo.nextInt();
+			if(k!=1){
+				System.out.println("Errrrrrrou");
+			}else{
+				System.out.println("Acertou Miserável");
+			}
+		}	
+
+		System.out.print("AQUELE OLÁ MUNDO CLÁSSICO PARA FINALIZAR A BAGAÇA\n");
 
 	}
 }
+
+
